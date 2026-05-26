606548bf931d666b54cf7968bb403155dfa68c0f
Omkar Yewale <omkarpatilyewale2004@gmail.com>
2026-05-26 13:05:14 +0530

    Min Max and Average of employee salary

--- may26.py
+++ may26.py
@@ -1,1 +1,16 @@
-#homwwork
+#find min ,max,and average salary of employee homework
+
+salary=[67000,45000,78000,55000,28000]
+max_sal=salary[0]
+min_sal=salary[0]
+sum=0
+for sal in salary:
+    sum=sum+sal
+    if sal>max_sal:
+        max_sal=sal
+    if sal<min_sal:
+        min_sal=sal
+average=sum/len(salary)
+print(f"max salary of employee is = {max_sal}")
+print(f"min salary of employee is = {min_sal}")
+print(f"Average salary of employee is ={average}")
