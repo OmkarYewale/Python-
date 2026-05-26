4a7a6163984099931762bc1949aff17ccdd462ea
Omkar Yewale <omkarpatilyewale2004@gmail.com>
2026-05-26 13:08:11 +0530

    total_sal

--- may26.py
+++ may26.py
@@ -3,14 +3,14 @@
 salary=[67000,45000,78000,55000,28000]
 max_sal=salary[0]
 min_sal=salary[0]
-sum=0
+total_sal=0
 for sal in salary:
-    sum=sum+sal
+    total_sal=total_sal+sal
     if sal>max_sal:
         max_sal=sal
     if sal<min_sal:
         min_sal=sal
-average=sum/len(salary)
+average=total_sal/len(salary)
 print(f"max salary of employee is = {max_sal}")
 print(f"min salary of employee is = {min_sal}")
 print(f"Average salary of employee is ={average}")
