fa85928c1564540d7a1beaef466b4095a7a2903d
Omkar Yewale <omkarpatilyewale2004@gmail.com>
2026-05-27 14:14:07 +0530

    Second min maxm

---
+++ HW2MinMax.py
@@ -0,0 +1,20 @@
+#Task :-> find second min and max
+salary=[67000,45000,78000,55000,28000]
+#First method
+salary.sort()
+print(F"second min number ={salary[1]}")
+print(f"second max number={salary[-2]}")
+
+#second method
+temp=0
+for i in range(0,len(salary)):
+    min=salary[i]
+    index=i
+    for j in range(i+1,len(salary)):
+        if min>salary[j]:
+            min=salary[j]
+            index=j
+    temp=salary[i]
+    salary[i]=min
+    salary[index]=temp
+print(salary)
